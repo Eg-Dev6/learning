8022f19c789050df37d1bcf061d01dba1ae0bdb7
Srour <egypt.team.dev6@gmail.com>
2024-12-19 10:48:55 +0200

    initial

---
+++ api/index.js
@@ -0,0 +1,12 @@
+module.exports = (req, res) => {
+    if (req.url === '/api/a' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('welcome to A Route');
+    } else if (req.url === '/api/b' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('welcome to B Route');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Route not found');
+    }
+};
